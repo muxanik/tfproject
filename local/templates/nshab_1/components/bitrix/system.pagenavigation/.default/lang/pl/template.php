f460362880cd1913f02a5e4e3e9f8f11b86a3590
muxanik <muxanik@gmail.com>
2016-12-02 02:04:53 +0300

    хз

--- local/templates/nshab_1/components/bitrix/system.pagenavigation/.default/lang/pl/template.php
+++
@@ -1,7 +0,0 @@
-<?
-$MESS ['nav_prev'] = "Poprz.";
-$MESS ['nav_paged'] = "Paginacja";
-$MESS ['pages'] = "Strony:";
-$MESS ['nav_next'] = "Następna";
-$MESS ['nav_all'] = "Szystkie";
-?>
--- local/templates/nshab_1/components/orion/ext.feedback.form/services/reload_captcha.php
+++
@@ -1,4 +0,0 @@
-<?php
-require($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_before.php');
-echo json_encode($APPLICATION->CaptchaGetCode()); 
-?>
